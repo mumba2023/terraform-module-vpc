556cf3089c4ad464b1a0938df0465c95f42fcad6
mumba <charlesmumba01@gmail.com>
2023-11-06 22:02:07 -0500

    created vpc module

---
+++ vpc/variables.tf
@@ -0,0 +1,13 @@
+# Environment variables
+variable "region" {}
+variable "project_name" {}
+variable "Environment" {}
+
+# vpc variables
+variable "vpc_cdr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
